1b815a9f96224b92fccdbcd6ccc27c339e1493a3
AleMMb <alejandramellab@gmail.com>
2022-11-24 09:48:28 -0300

    funcionalidad completa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import {BaseColaboradores} from './BaseColaboradores'
+import { useState } from 'react';
+
+
 
 function App() {
+
+  const [nombre, setNombre] = useState("");
+  const [correo, setCorreo] = useState("");
+  const [colaborador, setColaborador] = useState(BaseColaboradores)
+  const [busqueda, setBusqueda] = useState('')
+
+  const agragandoColaborador = (e) => {
+    e.preventDefault();
+    setColaborador([...colaborador, { "id": colaborador.length + 1, "nombre": nombre, "correo": correo}])
+    console.log(colaborador)
+    setNombre("")
+    setCorreo("")
+  }
+
+  const listaColaboradores = !busqueda ? colaborador : colaborador.filter((x) => x.nombre.toLowerCase().includes(busqueda.toLocaleLowerCase()))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className='buscador'>
+        <h3>Buscador de colaboradores</h3>
+        <input 
+            name='input-buscador' 
+            type="text" 
+            placeholder='Busca un colaborador' 
+            onChange={(e) => { setBusqueda(e.target.value); console.log(busqueda)}} value={busqueda}
+          />
+
+      </div>
+      <form className='formulario'>
+            <input 
+            name='nombre-formulario' 
+            type="text" 
+            placeholder='Ingresa nombre' 
+            onChange={(e) => { setNombre(e.target.value); }} value={nombre}
+            />
+
+
+            <input 
+            name='correo-formulario' 
+            type="text" 
+            placeholder='Ingresa e-mail' 
+            onChange={(e) => { setCorreo(e.target.value); }} value={correo}
+            />
+
+            <button onClick={agragandoColaborador}>Agregar</button>
+
+            <div className='lista'>
+              {listaColaboradores.map( colab => <li key={colab.id}>Nombre: {colab.nombre}correo: {colab.correo} ID: {colab.id}</li>)}
+            </div>
+        </form>
+    </main>
   );
 }
 
---
+++ src/BaseColaboradores.js
@@ -0,0 +1,33 @@
+export const BaseColaboradores = [
+    {
+    id: "1",
+    nombre: "Dolores De barriga",
+    correo: "meduele@lapanza.com"
+    },
+    {
+    id: "2",
+    nombre: "Pere Gil",
+    correo: "perejil@gmail.com"
+    },
+    {
+    id: "3",
+    nombre: "Armando Casas",
+    correo: "arquitectos@hotmail.com"
+    },
+    {
+    id: "4",
+    nombre: "Elton Tito",
+    correo: "micorreo@hotmail.com"
+    },
+    {
+    id: "5",
+    nombre: "Josefa Nática",
+    correo: "Btsloverforever@gmail.com"
+    },
+    {
+    id: "6",
+    nombre: "Helen Chufe",
+    correo: "tomacorriente123@hotmail.com"
+    },
+]
+    
